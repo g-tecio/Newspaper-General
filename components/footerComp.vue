88c081144f6710390bb759b7a3989e564acdd9a7
efradxxd <efradxxd@gmail.com>
2019-03-10 00:30:16 -0600

    add footer

---
+++ components/footerComp.vue
@@ -0,0 +1,140 @@
+<template>
+  <v-footer dark height="auto">
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+      integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+      crossorigin="anonymous"
+    >
+    <v-card class="flex" flat tile>
+      <v-card-title class="grey">
+        <v-flex xs12 md3>
+          <v-img
+            src="https://s3.amazonaws.com/nh-prototype/El+General+Oficial+500x168.png"
+            height="90px"
+            contain
+          ></v-img>
+        </v-flex>
+        <v-flex xs12 md1>
+          <ul class="footerLinks">
+            <li>
+              <a href>Home</a>
+            </li>
+            <li>
+              <a href>Discovery</a>
+            </li>
+            <li>
+              <a href>Photos</a>
+            </li>
+            <li>
+              <a href>Contact</a>
+            </li>
+          </ul>
+        </v-flex>
+
+        <v-flex xs12 md1>
+          <ul class="footerLinks">
+            <li>
+              <a href>About</a>
+            </li>
+            <li>
+              <a href>Help</a>
+            </li>
+            <li>
+              <a href>Terms</a>
+            </li>
+            <li>
+              <a href>Guidlines</a>
+            </li>
+          </ul>
+        </v-flex>
+        <v-flex xs12 md1>
+          <ul class="footerLinks">
+            <li>
+              <a href>Testimonials</a>
+            </li>
+            <li>
+              <a href>Adversite</a>
+            </li>
+            <li>
+              <a href>Integrations</a>
+            </li>
+            <li>
+              <a href>Carrers</a>
+            </li>
+          </ul>
+        </v-flex>
+        <v-flex xs12 md3></v-flex>
+        <v-flex xs12 md3>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-3" dark icon>
+            <a :href="icon.url" class="footerLinks">
+              <v-icon size="24px">{{ icon.icon }}</v-icon>
+            </a>
+          </v-btn>
+        </v-flex>
+        <!--
+        <strong class="subheading">Get connected with us on social networks!</strong>
+
+        <v-spacer></v-spacer>
+        -->
+      </v-card-title>
+
+      <v-card-actions class="grey justify-center">
+        &copy;2019 -
+        <strong>El General MX</strong>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
+</template>
+<style>
+.footerLinks {
+  list-style: none;
+  text-decoration: none;
+  color: #ffffff;
+}
+a {
+  color: #ffffff;
+  font-size: 10px;
+}
+a:link {
+  text-decoration: none;
+}
+a:visited {
+  color: #ffffff;
+}
+a:active {
+  color: #ffffff;
+}
+a:hover {
+  color: #ffffff;
+}
+</style>
+
+<script>
+import socialIcons from "~/components/social_icons/socialIcons.vue";
+export default {
+  data: () => ({
+    icons: {
+      link1: {
+        icon: "fab fa-facebook",
+        url: "www.facebook.com"
+      },
+      link2: {
+        icon: "fab fa-twitter",
+        url: "www.twitter.com"
+      },
+      link3: {
+        icon: "fab fa-linkedin",
+        url: "www.linkedin.com"
+      },
+      link4: {
+        icon: "fab fa-instagram",
+        url: "www.instagram.com"
+      }
+    }
+  }),
+  components: {
+    socialIcons
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,86 @@
 <template>
-  <v-container>
-    <headerComp/>
-    <navbar :config="config"/>
-    <banner/>
-    <drawer :config="config"/>
-    <v-layout row wrap>
-      <v-flex xs12 md9 id="newsContainer">
-        <newsContainer/>
-      </v-flex>
-      <v-flex xs12 md3 id="sideNews" class="hidden-sm-and-down">
-        <sideNews/>
-      </v-flex>
-      <v-flex xs12 md12 id="fullContainer">
-        <fullContainer/>
-      </v-flex>
-      <v-flex xs12 md9 id="afterFull">
-        <afterFullContainer/>
-      </v-flex>
-      <v-flex xs12 md3 id="sideNews" class="hidden-sm-and-down">
-        <sideNews2/>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div>
+    <v-container>
+      <headerComp/>
+
+      <navbar :config="config"/>
+
+      <banner/>
+
+      <drawer :config="config"/>
+      <!--
+      -->
+      <v-layout row wrap>
+        <v-flex xs12 md9 id="newsContainer">
+          <newsContainer/>
+        </v-flex>
+
+        <v-flex xs12 md3 id="sideNews" class="hidden-sm-and-down">
+          <sideNews/>
+        </v-flex>
+
+        <v-flex xs12 md12 id="fullContainer">
+          <fullContainer/>
+        </v-flex>
+
+        <v-flex xs12 md9 id="afterFull">
+          <afterFullContainer/>
+        </v-flex>
+
+        <v-flex xs12 md3 id="sideNews" class="hidden-sm-and-down">
+          <sideNews2/>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <footerComp/>
+  </div>
 </template>
 
 <script>
-import header from '~/components/navbar/header.vue'
-import navbar from '~/components/navbar/navbar.vue'
-import banner from '~/components/navbar/banner.vue'
-import drawer from '~/components/navbar/drawer.vue'
-import newsContainer from '~/components/newsContainer.vue'
-import sideNews from '~/components/sideNews.vue'
-import fullContainer from '~/components/fullContainer.vue'
-import afterFullContainer from '~/components/afterFullContainer.vue'
-import sideNews2 from '~/components/sideNews2.vue'
+import header from "~/components/navbar/header.vue";
+import navbar from "~/components/navbar/navbar.vue";
+import banner from "~/components/navbar/banner.vue";
+import drawer from "~/components/navbar/drawer.vue";
+import newsContainer from "~/components/newsContainer.vue";
+import sideNews from "~/components/sideNews.vue";
+import fullContainer from "~/components/fullContainer.vue";
+import afterFullContainer from "~/components/afterFullContainer.vue";
+import sideNews2 from "~/components/sideNews2.vue";
+import footerComp from "~/components/footerComp.vue";
 
-  export default {
-    data() {
-      return {
-        config:{
-          drawer:false
-        }
+export default {
+  data() {
+    return {
+      config: {
+        drawer: false
       }
-    },
-    components:{
-      'headerComp': header,
-      navbar,
-      banner,
-      drawer,
-      newsContainer,
-      sideNews,
-      fullContainer,
-      afterFullContainer,
-      sideNews2
-    }
-  } 
+    };
+  },
+  components: {
+    headerComp: header,
+    navbar,
+    banner,
+    drawer,
+    newsContainer,
+    sideNews,
+    fullContainer,
+    afterFullContainer,
+    sideNews2,
+    footerComp
+  }
+};
 </script>
 
 <style>
-  #newsContainer{
-    padding-top: 10px;
-    border-bottom: 2px solid #000;
-  }
-  #sideNews{
-    border-left: 1px solid #000;
-  }
-  #fullContainer{
-    border-top: 1px solid #000;
-    border-bottom: 1px solid #000;
-  }
+#newsContainer {
+  padding-top: 10px;
+  border-bottom: 2px solid #000;
+}
+#sideNews {
+  border-left: 1px solid #000;
+}
+#fullContainer {
+  border-top: 1px solid #000;
+  border-bottom: 1px solid #000;
+}
 </style>
